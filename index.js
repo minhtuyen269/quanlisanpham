074dc317d115db4bed1dbffb7b7694ec79ed49a0
minhtuyen248 <minhtuyen269@gmail.com>
2024-03-29 16:14:01 +0700

    HoanThanh

--- index.js
+++ index.js
@@ -1,55 +1,34 @@
-// let drinktr = document.querySelectorAll('.drink-tr');
-//
-// arr
-//
-// drinktr.forEach(function (tr) {
-//     tr.addEventListener('click', function () {
-//
-//         // Tạo một phần tử mới <li> để thêm vào danh sách order
-//         let listItem = document.createElement('li');
-//         listItem.textContent = tr.textContent;
-//
-//         // Thêm phần tử mới vào danh sách order
-//         document.getElementById('oderList').appendChild(listItem);
-//
-//     });
-// });
-//
-// // let dinkList = document.getElementById("drinktr");
-// // let searchDrinkName = document.getElementById("searchDrinkName");
-// //
-// // searchDrinkName.addEventListener("keyup", function () {
-// //     let searchItem = searchDrinkName.value.toLowerCase();
-// //
-// //     //lặp qua các sản phẩm và ẩn hiện các sản phẩm tương ứng dựa trên kết quả tìm kiem
-// //     Array.form(dinkList.getElementsByTagName("li")).forEach(function (drink) {
-// //         let drinkName = drink.textContent.toLowerCase();
-// //         if (drinkName.includes(searchItem)){
-// //             drink.style.display = "block"; //hiện sản phẩm nếu tìm thấy kết quả tương ứng
-// //         } else {
-// //             drink.style.display = "none"; //ẩn sản phẩm nếu không tìm thấy sản phẩm tương ứng
-// //         }
-// //     });
-// // });
-// // var drinks = [{id: 1, name: 'nuoc a', price: '123'}];
-// // let order = [{product_id: 1, quantity: 2}]
-//
 
 
 let drinks = [
-    {name: 'nuoc a', price: '123'},
-    {name: 'nuoc b', price: '1234'},
-    {name: 'nuoc c', price: '123'},
-    {name: 'nuoc d', price: '123'}
+    {name: 'Coffe nguyên chất', price: '29000'},
+    {name: 'Coffe sữa đá', price: '29000'},
+    {name: 'Coffe bạc xỉu', price: '29000'},
+    {name: 'Trà sen vàng', price: '45000'},
+    {name: 'Trà thạch đào', price: '45000'},
+    {name: 'Trà thạch vải', price: '45000'},
+    {name: 'Trà xanh đậu đỏ', price: '45000'},
+    {name: 'Freeze trà xanh', price: '55000'},
+    {name: 'Caramel phin Freeze', price: '55000'},
+    {name: 'Freeze Socola', price: '55000'},
+    {name: 'Bánh chuối', price: '25000'},
+    {name: 'Bánh phô mai chanh dây', price: '25000'},
+    {name: 'Bánh su kem', price: '25000'},
+    {name: 'Bánh Tiramisu', price: '25000'},
+    {name: 'Bánh Mousse đào', price: '25000'},
+    {name: 'Phindi hạnh nhân', price: '49000'},
+    {name: 'Phindi kem sữa', price: '49000'},
+    {name: 'Phindi choco', price: '49000'},
+
 ];
 
 function showAll(arr) {
     let html = '';
     for (let i = 0; i < arr.length; i++) {
         html += `<tr>
-                        <td>${arr[i].name}</td>
-                        <td>${arr[i].price}</td>
-                        <td><button onclick="addDrink('${arr[i].name}')">Thêm món</button></td>
+                        <th style="text-align: left">${arr[i].name}</th>
+                        <th>${arr[i].price}đ</th>
+                        <th><button onclick="addDrink('${arr[i].name}')">Thêm món</button></th>
                     </tr>`;
     }
     document.getElementById('list-drink').innerHTML = html;
@@ -101,10 +80,10 @@ function showOrder(order) {
     let html = "";
     for (let i = 0; i < order.length; i++) {
         html += `<tr>
-                        <td>${order[i].name}</td>
+                        <td style="text-align: left">${order[i].name}</td>
                         <td>${order[i].price}</td>
                         <td>${order[i].sl}</td>
-                        <td>${order[i].sl * order[i].price}</td>
+                        <td>${order[i].sl * order[i].price}đ</td>
                         <td><button onclick="deleteDrink('${order[i].name}')">Xóa</button></td>
                     </tr>`;
     }
